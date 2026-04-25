83de1f6e0014543c8e3c39ec2ff4cef9db951a97
caralmel <caralmel@alumni.uv.es>
2026-04-25 09:47:12 +0200

    He creado la función ptriang

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -21,3 +21,30 @@ dtriang <- function(x, min, max, mode) {
 
   res
 }
+
+#' Triangular distribution function
+#'
+#' @param q quantiles
+#' @param min lower bound
+#' @param max upper bound
+#' @param mode mode
+#' @return probabilities
+#' @export
+ptriang <- function(q, min, max, mode) {
+
+  if (min >= max) stop("min must be < max")
+  if (mode < min || mode > max) stop("mode must be between min and max")
+
+  res <- numeric(length(q))
+
+  left <- q >= min & q <= mode
+  right <- q > mode & q <= max
+
+  res[q < min] <- 0
+  res[q > max] <- 1
+
+  res[left] <- ((q[left] - min)^2) / ((max - min) * (mode - min))
+  res[right] <- 1 - ((max - q[right])^2) / ((max - min) * (max - mode))
+
+  res
+}
